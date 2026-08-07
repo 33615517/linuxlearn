769d6b70e861aa891432ff5d76b11841413016c0
星轨初途 <18567581539@163.com>
2026-08-07 10:35:11 +0800

    线程同步与阻塞以及消费者与生产者

--- threadsynchronization/CP/Main.cpp
+++ threadsynchronization/CP/Main.cpp
@@ -1,38 +1,36 @@
-#include"BlockQueue.hpp"
-#include"Task.hpp"
-#include<iostream>
-#include<ctime>
-#include<pthread.h>
-#include<unistd.h>
+#include "BlockQueue.hpp"
+#include "Task.hpp"
+#include <iostream>
+#include <ctime>
+#include <pthread.h>
+#include <unistd.h>
 
 void *consumer(void *arg)
 {
-        BlockQueue<task_t> *bq = static_cast<BlockQueue<task_t> *>(arg);
-    while(1)
+    BlockQueue<task_t> *bq = static_cast<BlockQueue<task_t> *>(arg);
+    while (1)
     {
         task_t t = bq->pop();
         t();
-        //std::cout << "消费者消费数据：" << t.X() << " + " << t.Y() << " = " << t.Result() << std::endl;
+        // std::cout << "消费者消费数据：" << t.X() << " + " << t.Y() << " = " << t.Result() << std::endl;
 
-       // sleep(1);
+        // sleep(1);
     }
- 
     return nullptr;
 }
 void *producter(void *arg)
 {
-    //int x = 1,y = 1;
+    // int x = 1,y = 1;
     BlockQueue<task_t> *bq = static_cast<BlockQueue<task_t> *>(arg);
-    while(1)
+    while (1)
     {
-           sleep(1);
+        sleep(1);
         std::cout << "生产了一个任务" << std::endl;
-        bq->Equeue(Download);  
+        bq->Equeue(Download);
 
-        // std::cout << "生产者生产数据：" << x << " + " << y << " = " << (x + y) << std::endl;  
+        // std::cout << "生产者生产数据：" << x << " + " << y << " = " << (x + y) << std::endl;
         // x++;
-        //y++;
-     
+        // y++;
     }
     return nullptr;
 }
@@ -40,41 +38,20 @@ void *producter(void *arg)
 int main()
 {
 
-    //申请阻塞队列
+    // 申请阻塞队列
     BlockQueue<task_t> *bq = new BlockQueue<task_t>();
 
-    
-    //构建生产者和消费者
-    pthread_t c,p;
-    pthread_create(&c,nullptr,consumer,bq);
-    pthread_create(&p,nullptr,producter,bq);
+    // 构建生产者和消费者
+    pthread_t c, p;
+    pthread_create(&c, nullptr, consumer, bq);
+    pthread_create(&p, nullptr, producter, bq);
 
-    pthread_join(c,nullptr);
-    pthread_join(p,nullptr);
+    pthread_join(c, nullptr);
+    pthread_join(p, nullptr);
 
     delete bq;
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // #include"BlockQueue.hpp"
 // #include"Task.hpp"
@@ -93,7 +70,7 @@ int main()
 //         std::cout << "消费者消费数据：" << t.X() << " + " << t.Y() << " = " << t.Result() << std::endl;
 //         sleep(1);
 //     }
- 
+
 //     return nullptr;
 // }
 // void *producter(void *arg)
@@ -103,12 +80,12 @@ int main()
 //     while(1)
 //     {
 //            sleep(1);
-        
-//         bq->Equeue(Task(x, y));  
-//         std::cout << "生产者生产数据：" << x << " + " << y << " = " << (x + y) << std::endl;  
+
+//         bq->Equeue(Task(x, y));
+//         std::cout << "生产者生产数据：" << x << " + " << y << " = " << (x + y) << std::endl;
 //         x++;
 //         y++;
-     
+
 //     }
 //     return nullptr;
 // }
@@ -119,7 +96,6 @@ int main()
 //     //申请阻塞队列
 //     BlockQueue<Task> *bq = new BlockQueue<Task>();
 
-    
 //     //构建生产者和消费者
 //     pthread_t c,p;
 //     pthread_create(&c,nullptr,consumer,bq);
